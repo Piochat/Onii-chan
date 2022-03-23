4eb21d27ccd9d01c34fbd3573e952a8c6be2ae42
Koke Gudiel <kokegudiel2@gmail.com>
2022-03-23 10:30:41 -0600

    Nothing Special

--- main.cpp
+++ main.cpp
@@ -4,55 +4,58 @@
 #include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <cstdlib>
 
-int main()
-{
-	// Create the main window
-	sf::RenderWindow window(sf::VideoMode(236, 406), "Te he infectado Onii-chan");
+int main() {
+  // Create the main window
+  sf::RenderWindow window(sf::VideoMode(236, 406), "Te he infectado Onii-chan");
 
-	// Load a sprite to display 
-	sf::Texture texture;
-	if (!texture.loadFromFile("resource/img/1.jpg"))
-		return EXIT_FAILURE;
-	sf::Sprite sprite(texture);
+  // Load a sprite to display
+  sf::Texture texture;
+  if (!texture.loadFromFile("resource/img/1.jpg"))
+    return EXIT_FAILURE;
+  sf::Sprite sprite(texture);
 
-	// Create a graphical text to display
-	sf::Font font;
-	if (!font.loadFromFile("resource/fonts/mplus-1mn-regular.ttf"))
-		return EXIT_FAILURE;
-	sf::Text text("Yamete\n Kudasai", font, 50);
+  // Create a graphical text to display
+  sf::Font font;
+  if (!font.loadFromFile("resource/fonts/mplus-1mn-regular.ttf"))
+    return EXIT_FAILURE;
+  sf::Text text("Yamete\n Kudasai", font, 50);
 
-	// Load a sound to play 
-	sf::Music music;
-	if (!music.openFromFile("resource/sounds/asus-yamete-kudasai.ogg"))
-		return EXIT_FAILURE;
+  // Load a sound to play
+  sf::Music music;
+  if (!music.openFromFile("resource/sounds/asus-yamete-kudasai.ogg"))
+    return EXIT_FAILURE;
 
-	// play music
-	music.play();
+  // play music
+  music.play();
 
-	// Start the program
-	while ( window.isOpen() ) {
-		// Process events
-		sf::Event event;
-		while ( window.pollEvent(event) ) {
-			// Close window: exit
-			if (event.type == sf::Event::Closed) 
-				window.close();
-		}
+  // Start the program
+  while (window.isOpen()) {
+    // Process events
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      // Close window: exit
+      if (event.type == sf::Event::Closed)
+        window.close();
+      if (event.type == sf::Event::KeyReleased &&
+          event.key.code == sf::Keyboard::Escape)
+        window.close();
+    }
 
-		/*
-		 * Clear 
-		 * Draw Sprite
-		 * Draw Text
-		 * Window deploy
-		 * */
-		window.clear();
-		window.draw(sprite);
-		window.draw(text);
-		window.display();
-	}
+    /*
+     * Clear
+     * Draw Sprite
+     * Draw Text
+     * Window deploy
+     * */
+    window.clear();
+    window.draw(sprite);
+    window.draw(text);
+    window.display();
+  }
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
